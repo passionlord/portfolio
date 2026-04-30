d24e4555130120afbcee1c8b967cd3f485448e63
vighnesh Raikar <raikar7178@gmail.com>
2026-04-30 12:44:49 +0530

    added additional column to track device name and the city , state

--- src/hooks/useVisitorCount.js
+++ src/hooks/useVisitorCount.js
@@ -27,24 +27,37 @@ function getDeviceInfo() {
   else if (/MSIE|Trident/i.test(ua)) browser = "IE";
 
   let os = "Unknown";
-  if (/Windows/i.test(ua))            os = "Windows";
-  else if (/Mac OS X/i.test(ua))      os = "macOS";
-  else if (/Android/i.test(ua))       os = "Android";
+  if (/Windows NT 10.0/i.test(ua))      os = "Windows 10";
+  else if (/Windows NT 6.3/i.test(ua))  os = "Windows 8.1";
+  else if (/Windows NT 6.2/i.test(ua))  os = "Windows 8";
+  else if (/Windows NT 6.1/i.test(ua))  os = "Windows 7";
+  else if (/Windows NT 5.1/i.test(ua))  os = "Windows XP";
+  else if (/Mac OS X (10[._]\d+)/i.test(ua)) os = ua.match(/Mac OS X (10[._]\d+)/i)[0].replace(/_/g, ".");
+  else if (/Mac OS X/i.test(ua))        os = "macOS";
+  else if (/Android/i.test(ua))         os = "Android";
   else if (/iPhone|iPad|iOS/i.test(ua)) os = "iOS";
-  else if (/Linux/i.test(ua))         os = "Linux";
+  else if (/Linux/i.test(ua))           os = "Linux";
 
   const referrer = document.referrer || "Direct";
 
-  return { device, browser, os, referrer };
+  // More descriptive device name
+  const device_name = `${os} ${browser} ${device}`;
+
+  return { device, browser, os, referrer, device_name };
 }
 
-async function getCountry() {
+
+async function getLocationInfo() {
   try {
     const res = await fetch("https://ipapi.co/json/", { signal: AbortSignal.timeout(3000) });
     const data = await res.json();
-    return data.country_name || "Unknown";
+    return {
+      country: data.country_name || "Unknown",
+      city: data.city || "Unknown",
+      region: data.region || "Unknown"
+    };
   } catch {
-    return "Unknown";
+    return { country: "Unknown", city: "Unknown", region: "Unknown" };
   }
 }
 
@@ -59,8 +72,8 @@ export function useVisitorCount() {
 
       if (!isOwner) {
         const sessionId = getOrCreateSessionId();
-        const { device, browser, os, referrer } = getDeviceInfo();
-        const country = await getCountry();
+        const { device, browser, os, referrer, device_name } = getDeviceInfo();
+        const { country, city, region } = await getLocationInfo();
 
         await supabase.from("visitors").upsert(
           {
@@ -69,7 +82,10 @@ export function useVisitorCount() {
             browser,
             os,
             device,
+            device_name,
             country,
+            city,
+            region,
             referrer,
           },
           { onConflict: "session_id" }
